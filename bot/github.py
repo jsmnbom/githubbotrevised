7fc997e05b58172581663a44bc1e9b1b15c0b3b0
Jasmin Bom <bomjacob@gmail.com>
2018-10-24 21:02:25 +0200

    Move sanitizer + add telegram serializer/truncator

--- bot/github.py
+++ bot/github.py
@@ -1,86 +1,19 @@
 import logging
 
-from bleach.sanitizer import Cleaner
-from html5lib.filters.base import Filter
 from telegram import ParseMode
 from telegram.ext import CallbackContext, Dispatcher
 
 from bot.githubapi import github_api
 from bot.githubupdates import GithubAuthUpdate, GithubUpdate
 from bot.menu import edit_menu_by_id
+from bot.utils import github_cleaner
 
 GITHUB_API_ACCEPT = {'Accept': 'application/vnd.github.machine-man-preview+json'}
 
 
-class _GithubFilter(Filter):
-    def __iter__(self):
-        in_quote = False
-        for token in super().__iter__():
-            if token['type'] == 'StartTag' and token['name'] == 'li':
-                if not (token['data'] and token['data'].get('class') != 'task-list-item'):
-                    yield {
-                        'data': '- ',
-                        'type': 'Characters'
-                    }
-            elif token['type'] == 'StartTag' and token['name'] == 'blockquote':
-                in_quote = True
-            elif token['type'] == 'EndTag' and token['name'] == 'blockquote':
-                in_quote = False
-            elif token['type'] == 'StartTag' and token['name'] == 'p':
-                if in_quote:
-                    yield {
-                        'data': '> ',
-                        'type': 'Characters'
-                    }
-            elif token['type'] == 'EmptyTag' and token['name'] == 'hr':
-                yield {
-                    'data': '\n────────────────────\n',
-                    'type': 'Characters'
-                }
-            elif token['type'] == 'EmptyTag' and token['name'] == 'input':
-                if token['data'].get('checked'):
-                    yield {
-                        'data': '☑ ',
-                        'type': 'Characters'
-                    }
-                else:
-                    yield {
-                        'data': '☐ ',
-                        'type': 'Characters'
-                    }
-            elif (token['type'] in ('StartTag', 'EndTag', 'EmptyTag') and
-                  token['name'] in ('li', 'blockquote', 'input', 'hr', 'p')):
-                pass
-            else:
-                yield token
-
-
-# This cleaner is not designed to use to transform content to be used in non-web-page contexts.
-# Is a warning from the bleach docs... that we are gonna totally ignore...
-# TODO: THIS IS NOT THREADSAFE
-_cleaner = Cleaner(
-    tags=[
-        'a',
-        'b',
-        'code',
-        'em',
-        'i',
-        'pre',
-        'strong',
-        'li',  # Stripped in _GithubFilter
-        'input',  # Stripped in _GithubFilter
-        'blockquote',  # Stripped in _GithubFilter
-        'p',  # Stripped in _GithubFilter
-        'hr'  # Stripped in _GithubFilter
-    ],
-    attributes={
-        'a': ['href'],
-        'li': ['class'],
-        'input': ['checked']
-    },
-    strip=True,
-    filters=[_GithubFilter]
-)
+def render_github_markdown(markdown, context: str):
+    html = github_api.markdown(markdown, context)
+    return github_cleaner.clean(html)
 
 
 class GithubHandler:
@@ -107,10 +40,10 @@ class GithubHandler:
     def handle_update(self, update: GithubUpdate, context: CallbackContext):
         return getattr(self, update.event, self.unknown)(update, context)
 
-    def unknown(self, update, context):
+    def unknown(self, update, _):
         self.logger.warning('Unknown event type %s. Data: %s', update.event, update.payload)
 
-    def ping(self, update, context):
+    def ping(self, update, _):
         self.logger.info('PING: %s', update.payload)
 
     def _iter_chat_ids(self, repository):
@@ -121,19 +54,14 @@ class GithubHandler:
                     if repo == repo_id:
                         yield chat_id
 
-    def render(self, markdown, context: str):
-        html = github_api.markdown(markdown, context)
-        return _cleaner.clean(html)
-
-    def issues(self, update, context):
+    def issues(self, update, _):
         # Issue opened, edited, closed, reopened, assigned, unassigned, labeled,
         # unlabeled, milestoned, or demilestoned.
         # TODO: Possibly support editing, closing, reopening, etc. of issues
         issue = update.payload['issue']
         repo = update.payload['repository']
 
-        text = self.render(issue['body'], repo['full_name'])
-        print(text)
+        text = render_github_markdown(issue['body'], repo['full_name'])
 
         for chat_id in self._iter_chat_ids(repo):
             self.dispatcher.bot.send_message(chat_id=chat_id, text=text, parse_mode=ParseMode.HTML)
--- bot/utils.py
+++ bot/utils.py
@@ -1,10 +1,16 @@
 import base64
 import hashlib
 import hmac
+import itertools
 import pickle
 from typing import Any
 
 import base65536
+import html5lib
+import telegram
+from bleach.sanitizer import Cleaner
+from html5lib.filters.base import Filter
+from html5lib.serializer import HTMLSerializer
 from telegram import MessageEntity
 
 from bot.const import HMAC_SECRET
@@ -67,3 +73,134 @@ def decode_first_data_entity(entities):
     for entity in entities:
         if entity.type == MessageEntity.TEXT_LINK and entity.url.startswith(URL_BASE):
             return decode_data_entity(entity)
+
+
+class _GithubFilter(Filter):
+    def __iter__(self):
+        in_quote = False
+        for token in super().__iter__():
+            if token['type'] == 'StartTag' and token['name'] == 'li':
+                if not (token['data'] and token['data'].get('class') != 'task-list-item'):
+                    yield {
+                        'data': '- ',
+                        'type': 'Characters'
+                    }
+            elif token['type'] == 'StartTag' and token['name'] == 'blockquote':
+                in_quote = True
+            elif token['type'] == 'EndTag' and token['name'] == 'blockquote':
+                in_quote = False
+            elif token['type'] == 'StartTag' and token['name'] == 'p':
+                if in_quote:
+                    yield {
+                        'data': '> ',
+                        'type': 'Characters'
+                    }
+            elif token['type'] == 'EmptyTag' and token['name'] == 'hr':
+                yield {
+                    'data': '\n────────────────────\n',
+                    'type': 'Characters'
+                }
+            elif token['type'] == 'EmptyTag' and token['name'] == 'input':
+                if token['data'].get('checked'):
+                    yield {
+                        'data': '☑ ',
+                        'type': 'Characters'
+                    }
+                else:
+                    yield {
+                        'data': '☐ ',
+                        'type': 'Characters'
+                    }
+            elif (token['type'] in ('StartTag', 'EndTag', 'EmptyTag') and
+                  token['name'] in ('li', 'blockquote', 'input', 'hr', 'p')):
+                pass
+            else:
+                yield token
+
+
+# "This cleaner is not designed to use to transform content to be used in non-web-page contexts."
+# ...is a warning from the bleach docs... that we are gonna totally ignore...
+# TODO: THIS IS NOT THREADSAFE
+# TODO: Does nested tags work? <b><i>test</i></b>
+github_cleaner = Cleaner(
+    tags=[
+        'a', 'b', 'code', 'em', 'i', 'pre', 'strong',
+        'li', 'input', 'blockquote', 'p', 'hr'  # Stripped in _GithubFilter
+    ],
+    attributes={
+        'a': ['href'],
+        'li': ['class'],
+        'input': ['checked']
+    },
+    strip=True,
+    filters=[_GithubFilter]
+)
+
+
+class TelegramTruncator(Filter):
+    def __init__(self, source, truncated_message, suffix,
+                 max_entities=telegram.constants.MAX_MESSAGE_ENTITIES,
+                 max_length=telegram.constants.MAX_MESSAGE_LENGTH):
+        super().__init__(source)
+        self.truncated_message = truncated_message or []
+        self.suffix = suffix or []
+        self.max_entities = max_entities
+        self.max_length = max_length
+
+    def __iter__(self):
+        for token in itertools.chain(self.truncated_message, self.suffix):
+            if token['type'] == 'StartTag':
+                self.max_entities -= 1
+            elif token['type'] in ('Characters', 'SpaceCharacters'):
+                self.max_length -= len(token['data'])
+
+        entity_count = 0
+        current_length = 0
+        current_tag_stack = []
+        for token in iter(self.source):
+            if entity_count >= self.max_entities:
+                for tag in reversed(current_tag_stack):
+                    yield {
+                        'type': 'EndTag',
+                        'name': tag
+                    }
+                    yield from iter(self.truncated_message)
+                break
+            if token['type'] in ('Characters', 'SpaceCharacters'):
+                if (current_length + len(token['data'])) > self.max_length:
+                    yield {
+                        'data': token['data'][:self.max_length - current_length],
+                        'type': 'Characters'
+                    }
+                    for tag in reversed(current_tag_stack):
+                        yield {
+                            'type': 'EndTag',
+                            'name': tag
+                        }
+                    for token2 in iter(self.truncated_message):
+                        yield token2
+                    break
+                else:
+                    current_length += len(token['data'])
+            elif token['type'] == 'EmptyTag':
+                entity_count += 1
+            elif token['type'] == 'StartTag':
+                entity_count += 1
+                current_tag_stack.append(token['name'])
+            elif token['type'] == 'EndTag':
+                current_tag_stack.pop()
+
+            yield token
+
+        for token in iter(self.suffix):
+            yield token
+
+
+def truncate(html, truncated_message, suffix):
+    walker = html5lib.getTreeWalker('etree')
+    html_stream = walker(html5lib.parseFragment(html, treebuilder='etree'))
+    truncated_message_stream = walker(html5lib.parseFragment(truncated_message, treebuilder='etree'))
+    suffix_stream = walker(html5lib.parseFragment(suffix, treebuilder='etree'))
+    truncated = TelegramTruncator(html_stream, truncated_message=truncated_message_stream, suffix=suffix_stream)
+    return HTMLSerializer().render(truncated)
+
